ba5985216efc4342b09d1430f13adc6305f8f367
korkhovpavel <korkhov.pi@gmail.com>
2021-01-15 22:52:30 +0300

    done (the decision is not mine): 1.4 step_10 1.6 step_7 2.1 step_7
    done:
    3.2 step_09

---
+++ 03_python_applications:_analyzing_text/02_regular_expressions_in_Python/09_step/task.py
@@ -0,0 +1,24 @@
+# Вам дана последовательность строк.
+# Выведите строки, содержащие две буквы "z", между которыми ровно три символа.
+# Sample Input:
+#
+# zabcz
+# zzz
+# zzxzz
+# zz
+# zxz
+# zzxzxxz
+# Sample Output:
+#
+# zabcz
+# zzxzz
+
+
+import sys
+import re
+
+pattern = r'z[a-z]{3}z'
+for line in sys.stdin:
+    line = line.rstrip()
+    if re.findall(pattern, line):
+        print(line)
